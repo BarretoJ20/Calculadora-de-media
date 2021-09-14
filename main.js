c765c0b84e548ab3209398af939b8ed9daa118c3
Júlia Barreto <barretojulia@id.uff.br>
2021-09-14 10:23:56 -0300

    Arredondando a nota para uma casa decimal

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ function notaFinal() {
   var notaFinal
   var n1 = parseFloat(document.getElementById('n1').value)
   var n2 = parseFloat(document.getElementById('n2').value)
-
   notaFinal = (n1 + n2) / 2
+  var notaArredondada = notaFinal.toFixed(1)
   console.log(n1 + n2) / 2
 
-  if (notaFinal >= 7)
-    alert('Parabéns, você foi aprovado com média ' + notaFinal)
-  if (notaFinal < 7)
+  if (notaArredondada >= 7)
+    alert('Parabéns, você foi aprovado com média ' + notaArredondada)
+  if (notaArredondada < 7)
     alert(
       'Infelizmente você não atingiu a média mínima 7. Sua média foi: ' +
-        notaFinal
+        notaArredondada
     )
 }
